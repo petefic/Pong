99296556db5798ce4f4b49d87f433c5b27753ed2
Peter Ficorelli <petefic@gmail.com>
2016-04-04 22:17:47 -0400

    player and ball movement

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+    public float speed = 30;
+
+    void Start()
+    {
+
+        int x = Random.Range(0, 2);
+        if (x == 0)
+        {
+            x = -1;
+        }
+        int z = Random.Range(-1, 2);
+        
+        Vector3 initalDir = new Vector3(x, 0, z);
+        
+        GetComponent<Rigidbody>().velocity = initalDir * speed;
+
+    }
+        
+}
---
+++ Assets/MovePaddle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePaddle : MonoBehaviour {
+
+    public float speed = 1f;
+
+	void Update()
+    {
+        float zPos = transform.position.z + (Input.GetAxis("Vertical") * speed);
+        Vector3 paddlePos = new Vector3(-15f, 1.1f, Mathf.Clamp(zPos, -7.5f, 7.5f));
+        transform.position = paddlePos;
+    }
+}
