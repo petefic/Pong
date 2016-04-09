d89dee1be78368278fd46cafe72a2de9e8fb7e84
Peter Ficorelli <petefic@gmail.com>
2016-04-08 20:07:50 -0400

    Ball respawning and scoring

---
+++ Assets/Ball.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 30; 
+
+    public void Awake()
+    {
+        //calc launch direction
+        int x = Random.Range(0, 2);
+        if (x == 0)
+        {
+            x = -1;
+        }
+        int z = Random.Range(-1, 2);
+        Vector3 initalDir = new Vector3(x, 0, z);
+
+        //launch
+        GetComponent<Rigidbody>().velocity = initalDir * speed;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        int x = 0;
+        if (col.gameObject.tag == "Paddle")
+        {
+            if (col.gameObject.name == "Player")
+            {
+                x = 1;
+            }
+            else //cpu paddle
+            {
+                x = -1;
+            }
+        }      
+        else if (col.gameObject.tag == "Goal")
+        {
+            GameObject gameController = GameObject.Find("GameController");
+            GameController gameControllerObj = gameController.GetComponent<GameController>();
+            if (col.gameObject.name == "Player Goal")
+            {
+                gameControllerObj.Goal("Player");
+            }
+            else //cpu goal
+            {
+                gameControllerObj.Goal("CPU");
+            }
+        }
+        else
+        {
+            return;
+        }
+
+        float z = GetCollisionPos(transform.position, col.transform.position, col.collider.bounds.size.z);
+        Vector3 dir = new Vector3(x, 0.5F, z).normalized;
+
+        GetComponent<Rigidbody>().velocity = dir * speed;
+    }
+
+    private float GetCollisionPos(Vector3 ballPos, Vector3 racketPos, float racketLength)
+    {
+        return ((ballPos.z - racketPos.z) / racketLength);
+    }
+        
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject ball;
+    public Text playerScoreText;
+    public Text cpuScoreText;
+
+    private int playerScore;
+    private int cpuScore;
+
+    void Start()
+    {
+        NewBall();
+    }
+
+    public void NewBall()
+    {
+        //create ball
+        Instantiate(ball, new Vector3(0, 0.5F, 0), Quaternion.identity);
+        Ball ballObj = ball.GetComponent<Ball>();
+    }
+
+    public void Goal(string scoredOn)
+    {
+        if (scoredOn == "Player")
+        {
+            cpuScore++;
+            cpuScoreText.text = cpuScore.ToString();
+        }
+        else
+        {
+            playerScore++;
+            playerScoreText.text = playerScore.ToString();
+        }
+
+        GameObject ball = GameObject.FindGameObjectWithTag("Ball");
+        Destroy(ball);
+
+        NewBall();
+    }
+
+}
